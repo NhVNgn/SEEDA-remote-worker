6631862fd3874da772a15095f12814a7ec38d820
dovsiien <679358Denis>
2020-11-03 22:51:11 -0500

    Change login ui, added simple welcome animation

--- app/src/main/java/com/example/project/ui/WelcomeActivity.java
+++ app/src/main/java/com/example/project/ui/WelcomeActivity.java
@@ -2,7 +2,11 @@ package com.example.project.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,23 +19,14 @@ public class WelcomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
-        setupSignUpButton();
-        setupSignInButton();
-    }
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.down_up);
+        ImageView image = findViewById(R.id.imageView);
+        image.startAnimation(animation);
 
-    private void setupSignUpButton() {
-        Button button = findViewById(R.id.signUpButtonWelcome);
-        button.setOnClickListener(v -> {
-            Intent intent = new Intent(this, RegisterActivity.class);
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
             startActivity(intent);
-        });
-    }
-
-    private void setupSignInButton() {
-        Button button = findViewById(R.id.signInButtonWelcome);
-        button.setOnClickListener(v -> {
-            Intent intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
-        });
+            finish();
+        }, 4000);
     }
 }
