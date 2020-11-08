d971fe7c0aaa1a0c8c831c00bda122efad7e481d
dovsiien <679358Denis>
2020-11-08 18:30:55 -0500

    Fixed minor bug

--- app/src/main/java/com/example/project/ui/ui_for_main/settings/SettingsFragment.java
+++ app/src/main/java/com/example/project/ui/ui_for_main/settings/SettingsFragment.java
@@ -20,7 +20,6 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_settings, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
         return root;
     }
 }
