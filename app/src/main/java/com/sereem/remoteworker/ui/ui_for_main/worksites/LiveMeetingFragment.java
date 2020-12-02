6cef2c810f7dde94fd4efc406964faffecf834b1
NhVyNgn <limousezhine23@gmail.com>
2020-12-01 22:05:35 -0800

    set up fragment

---
+++ app/src/main/java/com/sereem/remoteworker/ui/ui_for_main/worksites/LiveMeetingFragment.java
@@ -0,0 +1,24 @@
+package com.sereem.remoteworker.ui.ui_for_main.worksites;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sereem.remoteworker.R;
+
+public class LiveMeetingFragment extends Fragment {
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_live_meeting, container, false);
+    }
+}
